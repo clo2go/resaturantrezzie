425d4bed9029783946ba7f7f20f451e9d0206786
clo2go <clo2go@gmail.com>
2017-01-19 15:07:28 -0600

    require path, set routes, dixed __dirname

--- server.js
+++ server.js
@@ -5,21 +5,13 @@ var path = require('path');
 var app = express()
 var PORT = process.env.PORT || 8000;
  
-app.get('/', function (req, res) {
-  res.send('Hello World')
-})
- 
 // parse application/x-www-form-urlencoded 
 app.use(bodyParser.urlencoded({ extended: false }))
  
 // parse application/json 
 app.use(bodyParser.json())
- 
-app.use(function (req, res) {
-  res.setHeader('Content-Type', 'text/plain')
-  res.write('you posted:\n')
-  res.end(JSON.stringify(req.body, null, 2))
-})
+
+require('./app/routing/html-routes.js')(app);
 
 
 app.listen(PORT, function(){
